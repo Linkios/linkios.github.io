7f98701713132ef92945c1d06e86b3d7ea096443
Hoang Thieu <liberoht@gmail.com>
2018-07-12 13:28:46 +0700

    null key

--- content/js/redirect.js
+++ content/js/redirect.js
@@ -1,12 +1,14 @@
-
 redirect = {
     goto: function () {
         var url = new URL(window.location.href);
         var key = url.searchParams.get("go");
+        if (key == null) {
+            key = 'home';
+        }
         messageResource.init({
             filePath: 'content/config/'
         });
-        messageResource.load('config', function(){
+        messageResource.load('config', function () {
             var target = messageResource.get(key, 'config');
             console.log(target);
             window.location.replace(target);
